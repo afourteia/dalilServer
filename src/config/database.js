0a4de629f159f0704365e61f4ce01175eaba942c
Abdul <raoph40@gmail.com>
2023-03-05 18:37:48 -0500

    db connection issues

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // console.log("serverConfig.mongouri", serverConfig.mongouri);
+    console.log("serverConfig.mongouri", serverConfig.mongouri);
     await mongoose.connect(process.env.mongouri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB is Connnected");
+    console.log("MongoDB is connected");
   } catch (error) {
     console.error(error.message);
   }
