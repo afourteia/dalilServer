3a54527ccdd4c45d85092af48373fc8d1d7d4da4
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-06 16:14:01 +0500

    feature:Create controller functions for sms

---
+++ src/controllers/smsController.js
@@ -0,0 +1,58 @@
+const SmsServices = require("../services/smsServices");
+
+const CreateSms = async (req, res) => {
+  try {
+    const document = await SmsServices.createSms({
+      ...req.body,
+    });
+
+    const responseBody = {
+      codeStatus: "201",
+      message: "Sms created",
+      data: document,
+    };
+    return res.status(201).json({ ...responseBody });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const UpdateSms = async (req, res) => {
+  try {
+    const document = await SmsServices.updateSms(req.params.id, {
+      ...req.body,
+    });
+    if (!document) {
+      return res.status(404).json({ message: `sms not found` });
+    }
+    res.status(200).json(document);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const AllSms = async (req, res) => {
+  try {
+    let documents = SmsServices.getAllSms({});
+    if (documents.length === 0) {
+      message = "list is empty ";
+    }
+    const responseBody = {
+      codeStatus: "200",
+      message: message,
+      data: documents,
+    };
+
+    res.status(200).json({ ...responseBody });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = {
+  CreateSms,
+  UpdateSms,
+  AllSms,
+};
