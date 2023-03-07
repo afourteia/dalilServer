c8c6323ff8c52acfddfe791824349d8feb872126
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-07 16:50:07 +0500

    feature:Update config file

--- src/config/config.js
+++ src/config/config.js
@@ -4,10 +4,8 @@ dotenv.config({
   path: path.resolve(__dirname, `../../${process.env.NODE_ENV}.env`),
 });
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-console.log("process.env.NODE_ENV", process.env.PORT);
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
-  HOST: process.env.HOST || "localhost",
-  PORT: process.env.PORT || 3800,
+  PORT: process.env.PORT || 3000,
   mongouri: process.env.mongouri,
 };
