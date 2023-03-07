53ed80213eb241397ef337310b5d80ff8035821d
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-07 15:39:49 +0500

    feature:Create config file for environment variables setup

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({
+  path: path.resolve(__dirname, `../../${process.env.NODE_ENV}.env`),
+});
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+console.log("process.env.NODE_ENV", process.env.PORT);
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  HOST: process.env.HOST || "localhost",
+  PORT: process.env.PORT || 3800,
+  mongouri: process.env.mongouri,
+};
