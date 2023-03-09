224a49f601e0c1147fa33d921871cfecf5c8f07b
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-09 15:27:46 +0500

    create main route for subscriber

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -5,25 +5,23 @@ const loginRouter = require("./login.routes");
 const logoutRouter = require("./logout.routes");
 const usersRouter = require("./user.routes");
 const userRolesRouter = require("./userRole.routes");
-// const subscribersRouter = require("./subscriber.routes");
+const subscribersRouter = require("./subscriber.routes");
 const appointmentsRouter = require("./appointment.routes");
 const institutesRouter = require("./institution.routes");
 const medicalCentersRouter = require("./medicalCenter.routes");
 const schedulesRouter = require("./schedule.routes");
 
-const API_VERSION = "/v1"
+const API_VERSION = "/v1";
 
 router.use(API_VERSION + "/login", loginRouter);
 router.use(API_VERSION + "/logout", logoutRouter);
 router.use(API_VERSION + "/users", usersRouter);
 router.use(API_VERSION + "/userRoles", userRolesRouter);
-// router.use(API_VERSION + "/subscribers", subscribersRouter);
+router.use(API_VERSION + "/subscribers", subscribersRouter);
 router.use(API_VERSION + "/appointments", appointmentsRouter);
 router.use(API_VERSION + "/institutions", institutesRouter);
 router.use(API_VERSION + "/medicalCenters", medicalCentersRouter);
 router.use(API_VERSION + "/schedules", schedulesRouter);
 // router.use("/claims", claimsRouter);
-
-
 
 module.exports = router;
