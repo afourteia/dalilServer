67f80cb06caa213da0b976bf907958f3af9ff94c
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-09 15:26:02 +0500

    feature:Create route for csv upload

--- src/routers/subscriber.routes.js
+++ src/routers/subscriber.routes.js
@@ -5,16 +5,18 @@ const {
   getSubscriber,
   deleteSubscriber,
   getSubscribers,
+  createUsingCSV,
 } = require("../controllers/subscriberController");
 const { authentication } = require("../utilities/auth");
+const uploader = require("../utilities/uploader");
 var router = express.Router();
 
-router.get("",authentication, getSubscribers);
+router.post("/bycsv", uploader.uploads.any({ name: "file" }), createUsingCSV);
+router.get("", authentication, getSubscribers);
 router.post("", authentication, createSubscriber);
 router.get("/:subscriberId", authentication, getSubscriber);
 router.post("/:subscriberId", authentication, updateSubscriber);
 router.patch("/:subscriberId", authentication, updateSubscriber);
 router.delete("/:subscriberId", authentication, deleteSubscriber);
-
 
 module.exports = router;
