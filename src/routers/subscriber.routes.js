8e9b782992c819f23e2f384797e7448eedb0f2d1
Abdul <raoph40@gmail.com>
2023-03-13 02:57:03 -0400

    fix bugs

--- src/routers/subscriber.routes.js
+++ src/routers/subscriber.routes.js
@@ -5,13 +5,13 @@ const {
   getSubscriber,
   deleteSubscriber,
   getSubscribers,
-  createUsingCSV,
+  createSubscribersCSV,
 } = require("../controllers/subscriberController");
 const { authentication } = require("../utilities/auth");
 const uploader = require("../utilities/uploader");
 var router = express.Router();
 
-router.post("/uploadCSV", uploader.uploads.any({ name: "file" }), createUsingCSV);
+router.post("/uploadCSV", uploader.uploads.any({ name: "file" }), createSubscribersCSV);
 router.get("", authentication, getSubscribers);
 router.post("", authentication, createSubscriber);
 router.get("/:subscriberId", authentication, getSubscriber);
